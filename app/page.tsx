78c42f768f11a3600bb658ae43735123f63074cc
Joaquin Dovao <joaquin.dovao@dinocloudconsulting.com>
2024-02-06 16:17:43 -0300

    Test fetch with https and ec2 public ip

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [serverResponse, setServerResponse] = useState<string | null>(null);
 
   useEffect(() => {
-    const apiUrl = 'ec2-3-84-21-18.compute-1.amazonaws.com/';
+    const apiUrl = 'https://3.84.21.18/80';;
 
     fetch(apiUrl)
       .then(response => response.text())
