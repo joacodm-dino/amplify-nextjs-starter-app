de925522ef3a1a5e6b023b1b63b6a83052a67901
Joaquin Dovao <joaquin.dovao@dinocloudconsulting.com>
2024-02-06 15:54:15 -0300

    Test fetch with ec2 public domain

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [serverResponse, setServerResponse] = useState<string | null>(null);
 
   useEffect(() => {
-    const apiUrl = 'http://3.84.21.18:80/';
+    const apiUrl = 'ec2-3-84-21-18.compute-1.amazonaws.com';
 
     fetch(apiUrl)
       .then(response => response.text())
