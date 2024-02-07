8e8ee3dd8881ea83fa153031cb0a97f1b6021a3d
Joaquin Dovao <joaquin.dovao@dinocloudconsulting.com>
2024-02-07 13:43:06 -0300

    Test reach backend from frontend

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const [serverResponse, setServerResponse] = useState<string | null>(null);
 
   useEffect(() => {
-    const apiUrl = 'ec2-54-164-180-8.compute-1.amazonaws.com:8080/api';
+    const apiUrl = 'http://54.164.180.8:8080/api';
 
     fetch(apiUrl)
       .then(response => response.text())
