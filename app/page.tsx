e8342002a3d2ad25bee6b51ec1916edf9052d708
Joaquin Dovao <joaquin.dovao@dinocloudconsulting.com>
2024-02-08 12:01:39 -0300

    Fetch ec2 ip using server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,26 @@
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
 
 type ApiResponse = {
   // ajusta el tipo de datos según la respuesta real de tu servidor
   message: string;
-};
+}
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  try {
-    // Realizar la solicitud fetch al servidor
-    const apiUrl = 'http://3.84.21.18:80/';
-    const res = await fetch(apiUrl);
-    const data: ApiResponse = await res.json();
+export const getServerSideProps = (async () => {
+  // Realizar la solicitud fetch al servidor
+  const res = await fetch('http://3.84.21.18:80/)
+  const api: ApiResponse = await res.json()
 
-    // Retornar los datos como props
-    return {
-      props: {
-        data,
-      },
-    };
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    // Manejar errores y retornar un objeto de props vacío o un objeto de error
-    return {
-      props: {
-        error: 'Error fetching data',
-      },
-    };
-  }
-};
+  // Retornar los datos como props
+  return { props: { api } }
+})satisfies GetServerSideProps<{ api: ApiResponse }>
 
-function Page({
-  data,
-  error,
+export default function Page({
+  api,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <main>
-      {error ? (
-        <p>Error: {error}</p>
-      ) : (
-        <p>Server Response: {data.message}</p>
-      )}
+      <p>Server Response: {api.message}</p>
     </main>
   );
 }
-
-export default Page;
