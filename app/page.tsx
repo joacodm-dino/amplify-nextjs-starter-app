109f91b25153b0198dfec5398ed3f5b8432ed608
Joaquin Dovao <joaquin.dovao@dinocloudconsulting.com>
2024-02-08 14:47:03 -0300

    Fetch ec2 ip using server side

--- app/page.tsx
+++ app/page.tsx
@@ -25,68 +25,68 @@
 // }
 
 
-// async function getData() {
-//   const res = await fetch('http://54.174.190.209:80');
-//   // The return value is *not* serialized
-//   // You can return Date, Map, Set, etc.
- 
-//   if (!res.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error('Failed to fetch data');
-//   }
- 
-//   const contentType = res.headers.get('Content-Type');
-  
-//   if (contentType && contentType.includes('application/json')) {
-//     return res.json();
-//   } else {
-//     return res.text(); // or res.blob() if you want to handle binary data
-//   }
-// }
- 
-// export default async function Page() {
-//   try {
-//     const data = await getData();
-    
-//     // Check if data is JSON or HTML and handle accordingly
-//     if (typeof data === 'object') {
-//       // Handle JSON data
-//     } else {
-//       // Handle HTML data
-//       console.log('Received HTML:', data);
-//     }
-//   } catch (error) {
-//     console.error('Error fetching data:', error.message);
-//   }
-
-//   return <main></main>;
-// }
-
-
 async function getData() {
-  const url = 'http://54.174.190.209:80';
-
-  try {
-    const res = await fetch(url);
-
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
-
-    return res.text();
-  } catch (error) {
-    throw new Error(`Error fetching HTML data from ${url}: ${error.message}`);
+  const res = await fetch('http://54.174.190.209:80');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+ 
+  const contentType = res.headers.get('Content-Type');
+  
+  if (contentType && contentType.includes('application/json')) {
+    return res.json();
+  } else {
+    return res.text(); // or res.blob() if you want to handle binary data
   }
 }
-
+ 
 export default async function Page() {
   try {
-    const htmlData = await getData();
-    console.log('Received HTML:', htmlData);
-    // Puedes hacer más cosas con el HTML si es necesario
+    const data = await getData();
+    
+    // Check if data is JSON or HTML and handle accordingly
+    if (typeof data === 'object') {
+      // Handle JSON data
+    } else {
+      // Handle HTML data
+      console.log('Received HTML:', data);
+    }
   } catch (error) {
-    console.error('Error fetching HTML data:', error.message);
+    console.error('Error fetching data:', error.message);
   }
 
   return <main></main>;
 }
+
+
+// async function getData() {
+//   const url = 'http://54.174.190.209:80';
+
+//   try {
+//     const res = await fetch(url);
+
+//     if (!res.ok) {
+//       throw new Error('Failed to fetch data');
+//     }
+
+//     return res.text();
+//   } catch (error) {
+//     throw new Error(`Error fetching HTML data from ${url}: ${error.message}`);
+//   }
+// }
+
+// export default async function Page() {
+//   try {
+//     const htmlData = await getData();
+//     console.log('Received HTML:', htmlData);
+//     // Puedes hacer más cosas con el HTML si es necesario
+//   } catch (error) {
+//     console.error('Error fetching HTML data:', error.message);
+//   }
+
+//   return <main></main>;
+// }
