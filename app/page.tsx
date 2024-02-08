48f049a34f1fdf6579627acbd257054124a5d625
Joaquin Dovao <joaquin.dovao@dinocloudconsulting.com>
2024-02-08 12:06:18 -0300

    Fetch ec2 ip using server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
 
-
 type ApiResponse = {
   // ajusta el tipo de datos según la respuesta real de tu servidor
   message: string;
@@ -8,7 +7,7 @@ type ApiResponse = {
 
 export const getServerSideProps = (async () => {
   // Realizar la solicitud fetch al servidor
-  const res = await fetch('http://3.84.21.18:80/)
+  const res = await fetch('http://3.84.21.18:80/')
   const api: ApiResponse = await res.json()
 
   // Retornar los datos como props
